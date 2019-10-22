9b5b12ea184881283a15ba529d6598fb17c74a80
pauline <pauline.feytel@gmail.com>
2019-10-22 13:23:07 +0200

    Add FlatList for displaying the conversation

--- App.js
+++ App.js
@@ -1,13 +1,18 @@
 import React, { useState } from 'react';
-import { StyleSheet, View, TouchableWithoutFeedback, ScrollView } from 'react-native';
+import { StyleSheet, View, TouchableWithoutFeedback, FlatList } from 'react-native';
 import Sentence from './Components/Sentence';
 
 export default function App() {
   const [sentences, setSentences] = useState([
-    {id: 1, text: 'Hello!', position: 'right'},
-    {id: 2, text: 'How are you?', position: 'left'},
-    {id: 3, text: 'I am fine, and you?', position: 'right'},
-    {id: 4, text: 'Great, thanks!', position: 'left'}
+    {id: '1', text: 'Hello!', position: 'right'},
+    {id: '2', text: 'How are you?', position: 'left'},
+    {id: '3', text: 'I am fine, and you?', position: 'right'},
+    {id: '4', text: 'Great, thanks!', position: 'left'},
+    {id: '5', text: 'What are you doing now?', position: 'right'},
+    {id: '6', text: 'I work, and you?', position: 'left'},
+    {id: '7', text: 'I study', position: 'right'},
+    {id: '8', text: 'Where do you live?', position: 'left'},
+    {id: '9', text: 'I live in Stockholm!', position: 'right'},
   ]);
   const [index, setIndex] = useState(0);
   const [currentSentences, setCurrentSentences] = useState([]);
@@ -21,24 +26,27 @@ export default function App() {
     if (index >= sentences.length) {
       setIndex(0);
       setCurrentSentences([]);
-    }    
+    }
   }
 
   return (
-      <TouchableWithoutFeedback onPress={clickScreen}>
-        <View style={styles.container} onStartShouldSetResponder={() => true}>
-          <ScrollView>
-            {currentSentences.map((sentence, index) => <Sentence key={index} sentence={sentence.text} position={sentence.position} />)}
-          </ScrollView>
-        </View>
-      </TouchableWithoutFeedback>
+      
+    <TouchableWithoutFeedback onPress={clickScreen}>
+      <View style={styles.container}>
+        <FlatList
+          keyExtractor={(item, index) =>item.id}
+          data={currentSentences}
+          renderItem={itemData => (<Sentence style={styles.item} sentence={itemData.item.text} position={itemData.item.position}/>)}
+        />
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'white',
     paddingTop: 20,
   }
 });
